ec1f21b9d303e19dc92e34e773d44c8d7225f65f
allenabishekGithub <111240053+allenabishekGithub@users.noreply.github.com>
2023-04-04 16:29:19 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-public class hello {
+public class Hello {
    public static void main(String[] args) {
     System.out.println("Hello World - April 4th 2023 - IST 4:20pm - triggered by webhook");
     }
